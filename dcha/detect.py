cef7946f703988fb1db52232a1aff72a6f7986c0
Ray <ray.lee@daocha.me>
2017-10-16 02:19:25 +0800

    handling gpio on/off

--- dcha/detect.py
+++ dcha/detect.py
@@ -5,41 +5,33 @@ from actions.functions import Action as act
 
 
 gpio.setmode(gpio.BOARD)
-gpio.setup(24, gpio.IN, pull_up_down=gpio.PUD_UP)
-gpio.setup(26, gpio.IN, pull_up_down=gpio.PUD_UP)
+gpio.setup(24, gpio.IN)
+gpio.setup(26, gpio.IN)
 
 print("initializing...")
 
-def lighton_callback(channel):
+def light_callback(channel):
     print("Light detected...")
-    act.updateThing('True', None)
+    if gpio.input(channel):
+        act.updateThing('True', None)
+    else:
+        act.updateThing('False', None)
 
-def lightoff_callback(channel):
-    print("Light detected...")
-    act.updateThing('False', None)
-
-def shockon_callback(channel):
+def shock_callback(channel):
     print("Shocking detected...")
-    act.updateThing(None, 'True')
-    
-def shockoff_callback(channel):
-    print("Shocking detected...")
-    act.updateThing(None, 'False')
+    if gpio.input(channel):
+        act.updateThing(None, 'True')
+    else:
+        act.updateThing(None, 'False')
 
 def main():
     try:
         print("adding event listeners")
         # 26 for light sensor: light on
-        gpio.add_event_detect(26, gpio.RISING, callback=lighton_callback, bouncetime=3000)
-        
-        # 26 for light sensor: light off
-        gpio.add_event_detect(26, gpio.FALLING, callback=lightoff_callback, bouncetime=3000)
+        gpio.add_event_detect(26, gpio.BOTH, callback=light_callback, bouncetime=3000)
         
         # 24 for shock sensor: shocking
-        gpio.add_event_detect(24, gpio.RISING, callback=shockon_callback, bouncetime=1000)
-        
-        # 24 for shock sensor: stop shocking
-        gpio.add_event_detect(24, gpio.FALLING, callback=shockoff_callback, bouncetime=1000)
+        gpio.add_event_detect(24, gpio.BOTH, callback=shock_callback, bouncetime=1000)
         
         act.listenDelta()
     
