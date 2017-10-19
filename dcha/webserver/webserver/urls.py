43970c245ec6ef2c402f1a4b954da7ebb7f40b75
Ray <ray.lee@daocha.me>
2017-10-20 03:19:15 +0800

    change django app name

--- dcha/webserver/webserver/urls.py
+++ dcha/webserver/webserver/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     #url(r'^admin/', admin.site.urls),
-    url(r'^web/', include('iot_backend.urls')),
+    url(r'^device/', include('iot_backend.urls')),
 ]
