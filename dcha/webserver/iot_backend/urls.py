291fc785beefb5e3d49a440a5c504da5ff4f1aa2
Ray <ray.lee@daocha.me>
2017-10-20 04:22:46 +0800

    handle empty page : redirection

--- dcha/webserver/iot_backend/urls.py
+++ dcha/webserver/iot_backend/urls.py
@@ -5,7 +5,6 @@ from django.views.static import serve
 from . import views
 
 urlpatterns = [
-    #url(r'^$', views.index, name='index'),
-    url(r'^$', serve, {'path':'index.html', 'document_root': settings.STATIC_ROOT,}),
+    url(r'^$', views.index, name='index'),
     url(r'^check$', views.check_iot_status, name='check'),
 ]
--- dcha/webserver/iot_backend/views.py
+++ dcha/webserver/iot_backend/views.py
@@ -1,9 +1,9 @@
 from actions.functions import Action as act
 from django.http import HttpResponse
-
+from django.http import HttpResponseRedirect
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponseRedirect("/")
 
 def check_iot_status(request):
     return HttpResponse(act.getThingState())
