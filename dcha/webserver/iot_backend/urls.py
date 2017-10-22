6c82a855a26430ad7e65073b33ee4955fd021984
daocha <ray@daocha.me>
2017-10-22 23:00:53 +0800

    disable endpoint /check

--- dcha/webserver/iot_backend/urls.py
+++ dcha/webserver/iot_backend/urls.py
@@ -6,7 +6,7 @@ from .handler.requestHandler import ControlRequest
 
 urlpatterns = [
     url(r'^$', HomePageView.as_view(), name='home'),
-    url(r'^check$', views.check_iot_status, name='check'),
+    #url(r'^check$', views.check_iot_status, name='check'),
     url(r'^control$', ControlPanelView.as_view(), name='control_panel'),
     url(r'^control/request', ControlRequest.controlRequest, name='control_request'),
 ]
